f7e3997f15088bf3c26e419876267edbfe4bc2be
Hugo Chinga G <hugo.chinga.g@gmail.com>
2021-03-28 19:53:37 -0300

    apariencia de asignatura nivel

--- app/models/asignatura_nivel_base.rb
+++ app/models/asignatura_nivel_base.rb
@@ -1,7 +1,8 @@
 class AsignaturaNivelBase < ApplicationRecord
 
 	TABLA_FIELDS = [
-		['d_asignatura_nivel', 'show']
+		['d_asignatura', 'show'],
+		['d_nivel',    'normal']
 	]
 
 	belongs_to :asignatura_base
@@ -11,6 +12,14 @@ class AsignaturaNivelBase < ApplicationRecord
 
 	def d_asignatura_nivel
 		"#{self.asignatura_base.asignatura_base}#{' : ' if self.nivel_bases.any?}#{self.nivel_bases.map {|nb| nb.nivel_base}.join(' & ')}"
+	end
+
+	def d_asignatura
+		self.asignatura_base.asignatura_base
+	end
+
+	def d_nivel
+		self.nivel_bases.any? ? self.nivel_bases.map {|nb| nb.nivel_base}.join(' & ') : ""
 	end
 
 end
